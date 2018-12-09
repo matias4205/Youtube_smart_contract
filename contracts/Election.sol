021b5bbe52c0d727a5486f746ef294718f5e293f
matias4205 <matiasperezpc@hotmail.com>
2018-12-09 11:49:40 -0300

    Primera parte

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.2;
+
+contract Election{
+    // Store candidate
+    // Constructor
+    // Read candidate
+    string public candidate;
+
+    function Election() public{
+        candidate = "Candidate 1";
+    }
+}
